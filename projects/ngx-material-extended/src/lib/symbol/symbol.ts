362c2d7a4c78bc39492954ffc8d5a7e423ea6db6
Alexander Porrello <alexander.porrello@kapsch.net>
2026-04-24 16:18:16 -0500

    feat: add MexSymbol component and update tsconfig references

---
+++ projects/ngx-material-extended/src/lib/symbol/symbol.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'mex-symbol',
+    template: '<ng-content />',
+    encapsulation: ViewEncapsulation.None,
+    host: {
+        class: 'material-symbols-outlined material-symbols-sharp material-symbols-rounded'
+    }
+})
+export class MexSymbol {
+    constructor() {}
+}
